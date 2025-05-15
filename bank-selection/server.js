66e074739803b20295ce6f1f93dbe42dcd336da0
MMHRball <103367934+MMHRball@users.noreply.github.com>
2025-05-15 19:52:58 -0400

    Initial commit

--- bank-selection/server.js
+++ bank-selection/server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const colors = {
     reset: "\x1b[0m",
